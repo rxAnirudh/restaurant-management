86576a6019884e481ce1fb1bc128dc25304240fe
rxAnirudh <radixdt.2336@gmail.com>
2022-09-06 17:24:03 +0530

    🔨 Push changes related to urls and more changes in multi vendor project

--- foodOnline_main/foodOnline_main/urls.py
+++ foodOnline_main/foodOnline_main/urls.py
@@ -1,7 +1,7 @@
 """foodOnline_main URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from marketplace import views as MarketplaceViews
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.home, name='home'),
+    path('', include('accounts.urls')),
+
+    path('marketplace/', include('marketplace.urls')),
+
+    path('cart/', MarketplaceViews.cart, name='cart'),
+    path('search/', MarketplaceViews.search, name='search'),
+
+    path('checkout/', MarketplaceViews.checkout, name='checkout'),
+
+    path('orders/', include('orders.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
